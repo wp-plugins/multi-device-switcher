88e8265e795330ff5707165f6af8bb12cb210c25
thingsym <thingsym@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-03-05 06:51:32 +0000

    add file uninstall.php
    git-svn-id: https://plugins.svn.wordpress.org/multi-device-switcher/trunk@514475 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- multi-device-switcher.php
+++ multi-device-switcher.php
@@ -3,8 +3,8 @@
 Plugin Name: Multi Device Switcher
 Plugin URI: https://github.com/thingsym/multi-device-switcher
 Description: This WordPress plugin allows you to set a separate theme for device (Smart Phone, Tablet PC, Mobile Phone, Game).
-Version: 1.0.1
-Author: Yousuke Mizuno
+Version: 1.0.2
+Author: thingsym
 Author URI: http://www.thingslabo.com/
 License: GPL2
 */
@@ -134,10 +134,11 @@ class Multi_Device_Switcher {
 	}
 }
 
-$multi_device_switcher = new Multi_Device_Switcher;
+if ( ! is_admin() )
+	$multi_device_switcher = new Multi_Device_Switcher;
 
 /**
- * Properly enqueue styles and scripts for our multi_device_switcher options page.
+ * Properly enqueue scripts for our multi_device_switcher options page.
  *
  * This function is attached to the admin_enqueue_scripts action hook.
  *
@@ -145,10 +146,21 @@ $multi_device_switcher = new Multi_Device_Switcher;
  *
  */
 function multi_device_switcher_admin_enqueue_scripts( $hook_suffix ) {
-	wp_enqueue_style( 'multi-device-switcher-options', WP_PLUGIN_URL . '/multi-device-switcher/multi-device-switcher.css', false, '2011-08-22' );
-	wp_enqueue_style( 'thickbox', includes_url() . '/js/thickbox/thickbox.css', false, '20090514' );
 	wp_enqueue_script('jquery-ui-tabs');
 	wp_enqueue_script( 'multi-device-switcher-options', WP_PLUGIN_URL . '/multi-device-switcher/multi-device-switcher.js', array( 'jquery' ), '2011-08-22' );
+}
+
+/**
+ * Properly enqueue styles for our multi_device_switcher options page.
+ *
+ * This function is attached to the admin_enqueue_styles action hook.
+ *
+ * @since 1.0
+ *
+ */
+function multi_device_switcher_admin_enqueue_styles( $hook_suffix ) {
+	wp_enqueue_style( 'multi-device-switcher-options', WP_PLUGIN_URL . '/multi-device-switcher/multi-device-switcher.css', false, '2011-08-22' );
+	wp_enqueue_style( 'thickbox', includes_url() . '/js/thickbox/thickbox.css', false, '20090514' );
 }
 
 /**
@@ -220,7 +232,8 @@ function multi_device_switcher_add_page() {
 	if ($help) 
 		add_contextual_help( $theme_page, $help );
 
-	add_action( "admin_print_styles-$theme_page", 'multi_device_switcher_admin_enqueue_scripts' );
+	add_action( "admin_print_scripts-$theme_page", 'multi_device_switcher_admin_enqueue_scripts' );
+	add_action( "admin_print_styles-$theme_page", 'multi_device_switcher_admin_enqueue_styles' );
 }
 add_action( 'admin_menu', 'multi_device_switcher_add_page' );
 
--- readme.txt
+++ readme.txt
@@ -1,12 +1,12 @@
 === Multi Device Switcher ===
 
-Contributors: Yousuke Mizuno
+Contributors: thingsym
 Donate link: http://blog.thingslabo.com/archives/000251.html
 Link: https://github.com/thingsym/multi-device-switcher
 Tags: switcher, theme, ipad, iphone, android, tablet, mobile, game
 Requires at least: 3.2.1
 Tested up to: 3.3.1
-Stable tag: 1.0.1
+Stable tag: 1.0.2
 
 This WordPress plugin allows you to set a separate theme for device (Smart Phone, Tablet PC, Mobile Phone, Game).
 
@@ -24,7 +24,7 @@ This plugin detects if your site is being viewed by UserAgent and switches to se
 
 1. Unzip files.
 2. Upload "multi-device-switcher" to the "/wp-content/plugins/" directory.
-3. Activate the plugin through the 'Plugins' menu in WordPress
+3. Activate the plugin through the 'Plugins' menu in WordPress.
 4. Upload a separate theme to the "/wp-content/themes/" directory.
 5. Go to the "Multi Device Switcher" options page through the 'Appearance' menu in WordPress.
 6. Configure settings to your needs. Select Theme by Theme option. Add and fix UserAgent by UserAgent option if necessary.
@@ -32,6 +32,10 @@ This plugin detects if your site is being viewed by UserAgent and switches to se
 
 == Changelog ==
 
+= 1.0.2 =
+* added: add file uninstall.php
+* fixed: split admin_enqueue_scripts() into two functions
+* fixed: detects is_admin()
 = 1.0.1 =
 * fixed: split multi_device_switcher_init() into two functions
 = 1.0.0 =
